4a8f100785e48c4a2a352fd920f20261dab805c8
Christian <dev@pojoni.at>
2025-06-19 19:57:54 +0200

    include session name

--- telegram_manager/__init__.py
+++ telegram_manager/__init__.py
@@ -5,19 +5,25 @@ from .controller import (
 
 
 class TelegramManager(_TelegramManager):
-    def __init__(self):
-        from .config import Config
-
-        super().__init__(Config.TELEGRAM_API_ID, Config.TELEGRAM_API_HASH, Config.TELEGRAM_PHONE_NUMBER)
-
-class AsyncTelegramManager(_AsyncTelegramManager):
-    def __init__(self):
+    def __init__(self, session_name: str = "session"):
         from .config import Config
 
         super().__init__(
             Config.TELEGRAM_API_ID,
             Config.TELEGRAM_API_HASH,
             Config.TELEGRAM_PHONE_NUMBER,
+            session_name=session_name,
+        )
+
+class AsyncTelegramManager(_AsyncTelegramManager):
+    def __init__(self, session_name: str = "session"):
+        from .config import Config
+
+        super().__init__(
+            Config.TELEGRAM_API_ID,
+            Config.TELEGRAM_API_HASH,
+            Config.TELEGRAM_PHONE_NUMBER,
+            session_name=session_name,
         )
 
 
